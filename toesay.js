a9fdd7ee5353cffd74a8a04704f1f60fc2515100
Tech-a-Tech <33734119+Tech-a-Tech@users.noreply.github.com>
2017-12-16 11:23:48 -0500

    v2.0.2
    Toesay source

---
+++ toesay.js
@@ -0,0 +1,10 @@
+module.exports = function toesay(message = "") {
+  return `                         (                            )
+  ooooooooooooo             (                                )
+  (           ) ------- (           ${message}                     )
+  (           ) ------- (                             )
+  (           )                 (                       )
+  (           )                      (             )
+  vvvvvvvvvvvvv
+  `;
+}
