4cae174b019d8d0500f6dabf9ae996abd3e52aa5
Tech-a-Tech <33734119+Tech-a-Tech@users.noreply.github.com>
2017-12-27 08:06:16 -0800

    v2.0.2
    Small color base file

--- hexes.js
+++ hexes.js
@@ -3,7 +3,7 @@ function generate() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-async function invertColor(color) {
+function invertColor(color) {
   try {
     return invert(color);
   } catch (err) {
